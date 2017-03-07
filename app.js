39d32c838e2361d27c3620749956b0a5e13bc215
Ravi Prasad <Ravi Prasad>
2017-03-07 17:54:24 +0530

    successful CI

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins successful');
+  res.send('hello jenkins successful from user');
 });
  
 app.listen(process.env.PORT || 5000);
